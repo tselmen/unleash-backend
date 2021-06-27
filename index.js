3146c020244503e210f7eb1dbba2863fa8a21e6d
Tselmen <tselmen@mn.data-artist.com>
2021-06-27 11:27:24 +0800

    Added mongoose and User model

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { ApolloServer, gql } = require('apollo-server-express');
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
 const typeDefs = gql`
   type Query {
     hello: String
+    helloWorld: String
   }
 `;
 
 const resolvers = {
-  Query: {
-    hello: () => 'Hello world!'
-  },
+    Query: {
+        hello: () => 'Hello world!',
+        helloWorld: () => 'Hello World!'
+    },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
 
-app.listen({ port: PORT }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-)
+mongoose
+    .connect('mongodb://localhost:27017/unleash', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log("MongoDB connected!");
+        return app.listen({ port: PORT }, () =>
+            console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+        )})
+    .catch(error => {
+        console.error(error)
+    })
